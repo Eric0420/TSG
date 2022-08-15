abc106fe4ba5e3c51e966bd114e0b6301f78d118
Eric CHEN <baojianc@utas.edu.au>
2022-08-15 16:32:23 +1000

    Update the Search function

---
+++ app/src/main/java/com/kit301/tsgapp/ui/search/ListProductDetails.kt
@@ -0,0 +1,289 @@
+package com.kit301.tsgapp.ui.search
+
+import android.app.Activity
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.kit301.tsgapp.DrawerBaseActivity
+import com.kit301.tsgapp.Product
+import com.kit301.tsgapp.databinding.ActivityProductDetailsBinding
+import com.kit301.tsgapp.ui.test.FIREBASE_TAG
+import java.io.File
+import java.util.*
+
+//Connect to DB and retrieve the product details
+val db = Firebase.firestore
+
+//This variable use to control the action of the Favourite button
+var isMyFavourite : Boolean = false
+
+@Suppress("DEPRECATION")
+class ListProductDetails : DrawerBaseActivity() {
+
+    private lateinit var ui : ActivityProductDetailsBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityProductDetailsBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        setActionbarTitle()
+
+        var favouriteProductID = -1
+        var favouriteProductObject = Product()
+        //Retrieve the product extra information from previous page
+        if(filterOn){
+            favouriteProductID = intent.getIntExtra(FilterProductIndex, -1)
+            favouriteProductObject = filtered[favouriteProductID]
+        }else{
+            favouriteProductID = intent.getIntExtra(ListProductIndex, -1)
+            favouriteProductObject = ProductItems[favouriteProductID]
+        }
+
+
+        val barcodeNumber = favouriteProductObject.BarcodeNumber
+
+        //Setup the user interface
+        setupUI(favouriteProductObject)
+
+        //Check the product favourite status
+        checkFavouriteStatus(favouriteProductObject)
+
+        ui.btnFavourite.setOnClickListener {
+
+            //If the current product exist in favourite list
+            if (isMyFavourite) {
+                ui.btnFavourite.backgroundTintList = ColorStateList.valueOf(Color.BLACK)
+                isMyFavourite = false
+
+                //Delete the product from favourite database
+                deleteFavouriteProduct(barcodeNumber)
+            }
+
+            //If the current product not exist in favourite list
+            else {
+                ui.btnFavourite.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                isMyFavourite = true
+
+                //Add the product to favourite database
+                addFavouriteProduct(barcodeNumber)
+
+            }
+
+        }
+        //Share product image
+        ui.btnShare.setOnClickListener {
+            this.window.decorView.isDrawingCacheEnabled = true
+            val bmp: Bitmap = this.window.decorView.drawingCache
+            val uri= Uri.parse(MediaStore.Images.Media.insertImage(contentResolver, bmp, "IMG"+ Calendar.getInstance().time, null))
+            val intent=Intent(Intent.ACTION_SEND)
+            intent.type = "image/jpeg"
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            startActivity(Intent.createChooser(intent, "Share"))
+        }
+
+    }
+
+    private fun setActionbarTitle() {
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val currentLanguageSetting = sharedPreferences.getString("My_Lang", "")
+        if (currentLanguageSetting == "en" || currentLanguageSetting == ""){
+            allocateActivityTitle("Product Details")
+        }
+        else if (currentLanguageSetting == "zh"){
+            allocateActivityTitle("商品详细信息")
+        }
+    }
+
+
+    private fun addFavouriteProduct(barcodeNumber: String?) {
+
+        val enProductsCollection = db.collection("enProduct")  //The English Language product database
+        val zhProductsCollection = db.collection("zhProduct")  ////The Chinese Language product database
+        val deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID)
+
+        //Add the English Version Data to the UserFavouriteProduct Database
+        enProductsCollection
+                .whereEqualTo("barcodeNumber","$barcodeNumber")  //This is a Query for searching the document which barcodeNum is: "${barcodeNum}"
+                .get()
+                .addOnSuccessListener { result->
+                    for (document in result) {
+                        val favouriteProduct = document.toObject<Product>()
+
+                        val favouriteProductCollection = db.collection("UserFavouriteProduct")
+                        favouriteProductCollection.document(deviceID)
+                                .collection("en")
+                                .document("${favouriteProduct.Name}")
+                                .set(favouriteProduct)
+                                .addOnSuccessListener {
+                                    //Display the text to tell the user that the action is success
+                                    Toast.makeText(this, "Successfully added to you favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.d(FIREBASE_TAG, "Document created with product name: ${favouriteProduct.Name}")
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(this, "Failed to add to favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.e(FIREBASE_TAG, "Error writing document")
+                                }
+
+                    }
+                }
+
+        //Add the Chinese Version Data to the UserFavouriteProduct Database
+        zhProductsCollection
+                .whereEqualTo("barcodeNumber","$barcodeNumber")  //This is a Query for searching the document which barcodeNum is: "${barcodeNum}"
+                .get()
+                .addOnSuccessListener { result->
+                    for (document in result) {
+                        val favouriteProduct = document.toObject<Product>()
+
+                        val favouriteProductCollection = db.collection("UserFavouriteProduct")
+                        favouriteProductCollection.document(deviceID)
+                                .collection("zh")
+                                .document("${favouriteProduct.Name}")
+                                .set(favouriteProduct)
+                                .addOnSuccessListener {
+                                    //Display the text to tell the user that the action is success
+                                    Toast.makeText(this, "Successfully added to you favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.d(FIREBASE_TAG, "Document created with product name: ${favouriteProduct.Name}")
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(this, "Failed to add to favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.e(FIREBASE_TAG, "Error writing document")
+                                }
+
+                    }
+                }
+
+    }
+
+    private fun deleteFavouriteProduct(barcodeNumber: String?) {
+
+        val enProductsCollection = db.collection("enProduct")  //The English Language product database
+        val zhProductsCollection = db.collection("zhProduct")  //The Chinese Language product database
+        val deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID)
+
+        //Delete the English Version Data from the UserFavouriteProduct Database
+        enProductsCollection
+                .whereEqualTo("barcodeNumber","$barcodeNumber")  //This is a Query for searching the document which barcodeNum is: "${barcodeNum}"
+                .get()
+                .addOnSuccessListener { result->
+                    for (document in result) {
+                        val favouriteProduct = document.toObject<Product>()
+
+                        val favouriteProductCollection = db.collection("UserFavouriteProduct")
+                        favouriteProductCollection.document(deviceID)
+                                .collection("en")
+                                .document("${favouriteProduct.Name}")
+                                .delete()
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "Successfully remove the product from your favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.d(FIREBASE_TAG, "Successfully deleted with product name: ${favouriteProduct.Name}")
+                                }
+                                .addOnFailureListener {
+                                    Log.e(FIREBASE_TAG, "Error in deleting document")
+                                }
+
+
+                    }
+                }
+
+        //Delete the Chinese Version Data from the UserFavouriteProduct Database
+        zhProductsCollection
+                .whereEqualTo("barcodeNumber","$barcodeNumber")  //This is a Query for searching the document which barcodeNum is: "${barcodeNum}"
+                .get()
+                .addOnSuccessListener { result->
+                    for (document in result) {
+                        val favouriteProduct = document.toObject<Product>()
+
+                        val favouriteProductCollection = db.collection("UserFavouriteProduct")
+                        favouriteProductCollection.document(deviceID)
+                                .collection("zh")
+                                .document("${favouriteProduct.Name}")
+                                .delete()
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "Successfully remove the product from your favourite list", Toast.LENGTH_SHORT).show()
+                                    Log.d(FIREBASE_TAG, "Successfully deleted with product name: ${favouriteProduct.Name}")
+                                }
+                                .addOnFailureListener {
+                                    Log.e(FIREBASE_TAG, "Error in deleting document")
+                                }
+
+
+                    }
+                }
+
+    }
+
+    private fun checkFavouriteStatus(favouriteProductObject: Product) {
+
+        val deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID)
+
+        //Get the current language setting (Check which language is using)
+        var currentLanguage = getSharedPreferences("Settings", Activity.MODE_PRIVATE).getString("My_Lang", "")
+
+        //This if statement is used to prevent error (If do not add this statement, when user do not change the language setting, the currentLanguage value will be null)
+        if (currentLanguage == "en" || currentLanguage == ""){
+            currentLanguage = "en"
+        }
+
+        //Check whether or not the current product is in favourite list
+        val checkFavouriteStatus = FirebaseFirestore.getInstance().collection("UserFavouriteProduct")
+                .document(deviceID).collection("$currentLanguage")
+                .document("${favouriteProductObject.Name}")
+        checkFavouriteStatus.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null) {
+                    if (document.exists()) {
+                        ui.btnFavourite.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                        isMyFavourite = true
+                    } else {
+                        isMyFavourite = false
+                    }
+                }
+            } else {
+                Log.d("TAG", "Error: ", task.exception)
+            }
+        }
+
+    }
+
+    private fun setupUI(favouriteProductObject: Product) {
+        //Setup the user interface
+        ui.productName.text =favouriteProductObject.Name
+        ui.productABV.text = favouriteProductObject.ABV
+        ui.productYear.text = favouriteProductObject.YearOfRelease.toString()
+        ui.productPrice.text = favouriteProductObject.AverageSalesPrice.toString()
+        ui.productType.text = favouriteProductObject.Type
+        ui.TasteNose.text = favouriteProductObject.TasteNose
+        ui.TastePalate.text = favouriteProductObject.TastePalate
+        ui.TasteFinish.text = favouriteProductObject.TasteFinish
+        ui.productVolume.text = favouriteProductObject.Volume.toString()
+
+        //Retrieve the image from DB
+        val storageRef = FirebaseStorage.getInstance().reference.child("Images/${favouriteProductObject.Image}")
+        val localfile = File.createTempFile("tempImage", "jpg")
+        storageRef.getFile(localfile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+            ui.targetImage.setImageBitmap(bitmap)
+        }.addOnFailureListener {
+            //Toast.makeText(this, "Failed to retrieve the image", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+}
--- app/src/main/java/com/kit301/tsgapp/ui/search/SearchProduct.kt
+++ app/src/main/java/com/kit301/tsgapp/ui/search/SearchProduct.kt
@@ -2,12 +2,34 @@ package com.kit301.tsgapp.ui.search
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.kit301.tsgapp.DrawerBaseActivity
+import com.kit301.tsgapp.Product
 import com.kit301.tsgapp.databinding.ActivitySearchProductBinding
-import com.kit301.tsgapp.ui.settings.SettingsActivity
+import com.kit301.tsgapp.databinding.MyFavouriteListBinding
+import com.kit301.tsgapp.ui.test.FIREBASE_TAG
+import java.io.File
 import java.util.*
+
+const val ListProductIndex = "ListProduct_Index"
+const val FilterProductIndex = "FilterProduct_Index"
+val ProductItems = mutableListOf<Product>()
+var filtered = mutableListOf<Product>()
+var filterOn = false
 
 class SearchProduct : DrawerBaseActivity() {
 
@@ -19,6 +41,120 @@ class SearchProduct : DrawerBaseActivity() {
         setContentView(ui.root)
         setActionbarTitle()
 
+        //Link the RecyclerView to your adapter
+        ui.productList.adapter = ListProductAdapter(ListProduct = ProductItems)
+
+        //Tell your RecyclerView how to display the items, vertical list
+        //ui.productList.layoutManager = LinearLayoutManager(this)
+        //or a grid layout
+        ui.productList.layoutManager = GridLayoutManager(this,2)
+
+        //Get database connection and connect to database
+        val db = Firebase.firestore
+        var languageFlag = "enProduct"
+        filterOn = false
+
+        if (getSharedPreferences("Settings", Activity.MODE_PRIVATE).getString("My_Lang", "") == "en" || getSharedPreferences("Settings", Activity.MODE_PRIVATE).getString("My_Lang", "") == ""){
+            languageFlag = "enProduct"
+        }else{
+            languageFlag = "zhProduct"
+        }
+
+        //Retrieve data from database
+        var productCollection = db.collection(languageFlag)
+        productCollection
+                .get()
+                .addOnSuccessListener { result ->
+                    ProductItems.clear() //this line clears the list, and prevents a bug where items would be duplicated upon rotation of screen
+                    Log.d(FIREBASE_TAG, "--- all Product ---")
+                    for (document in result)
+                    {
+                        //Log.d(FIREBASE_TAG, document.toString())
+                        val product = document.toObject<Product>()
+                        product.id = document.id
+                        Log.d(FIREBASE_TAG, product.toString())
+
+                        ProductItems.add(product)
+                    }
+                    (ui.productList.adapter as SearchProduct.ListProductAdapter).notifyDataSetChanged()
+                }
+
+        ui.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // on below line we are checking
+                // if query exist or not.
+
+                if (query != null) {
+                    // if query exist within list we
+                    // are filtering our list adapter.
+                    filtered = ProductItems.filter { it.Name?.contains(query) ?: false }.toMutableList()
+                    ui.productList.adapter = ListProductAdapter(ListProduct = filtered)
+                    filterOn = true
+                } else {
+                    // if query is not present we are displaying
+                    // a toast message as no  data found..
+                    Toast.makeText(baseContext, "No product found..", Toast.LENGTH_LONG)
+                            .show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // if query text is change in that case we
+                // are filtering our adapter with
+                // new text on below line.
+                return false
+            }
+
+        })
+    }
+
+
+    inner class ListProductHolder(var ui: MyFavouriteListBinding) : RecyclerView.ViewHolder(ui.root) {}
+
+    //The adapter is the controller that handles the communication between our model and our view.
+    inner class ListProductAdapter(private val ListProduct: MutableList<Product>) : RecyclerView.Adapter<ListProductHolder>() {
+
+        //inflates a new row, and wraps it in a new ViewHolder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchProduct.ListProductHolder {
+            val ui = MyFavouriteListBinding.inflate(layoutInflater, parent, false)   //inflate a new row from the my_favourite_list.xml
+            return ListProductHolder(ui)                                             //wrap it in a ViewHolder
+        }
+
+        override fun onBindViewHolder(holder: ListProductHolder, position: Int) {
+            val product = ListProduct[position]   //get the data at the requested position
+            holder.ui.txtFavouriteProductName.text = product.Name
+
+            val storageImage = FirebaseStorage.getInstance().reference.child("Images/${product.Image}")
+            val localImage = File.createTempFile("tempImage", "jpg")
+            storageImage.getFile(localImage).addOnSuccessListener {
+                val bitmap = BitmapFactory.decodeFile(localImage.absolutePath)
+                holder.ui.FavouriteProductImage.setImageBitmap(bitmap)
+
+            }.addOnFailureListener{
+                //Toast.makeText(this, "Failed to retrieve the image", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+
+            holder.itemView.setOnClickListener {
+
+                var intent = Intent(holder.itemView.context, ListProductDetails::class.java)
+
+                if (filterOn){
+                    intent.putExtra(FilterProductIndex, position)
+                }else{
+                    intent.putExtra(ListProductIndex, position)
+                }
+                startActivity(intent)
+
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return ListProduct.size
+        }
     }
 
     private fun setActionbarTitle() {
