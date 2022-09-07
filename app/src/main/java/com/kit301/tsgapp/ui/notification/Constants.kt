5cbd391f00034bee974cba137f7061b02eafdbe8
Eric CHEN <baojianc@utas.edu.au>
2022-09-07 19:53:06 +1000

    Revert "Delete Constants.kt"
    This reverts commit 573bb9c4

---
+++ app/src/main/java/com/kit301/tsgapp/ui/notification/Constants.kt
@@ -0,0 +1,11 @@
+package com.kit301.tsgapp.ui.notification
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAA594OF8w:APA91bFB_We-SaFzKyZUzgll78oirk7kHZKTrr_3IXnlS5Q-CiM_kEwCEgXK2z76EA_KZN3Hx5tqhsxox2neIr-cBwlBCuOlzwMlXTGcEY5qZw754b3k4AZXQxN_asW6RvW_i6rl_LGw"
+        const val CONTENT_TYPE = "application/json"
+
+    }
+
+}
